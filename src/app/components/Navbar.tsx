db2049a7997221750ff0dd7329f7c5b69c5a1c3e
Ornella <nelitagrigolato@gmail.com>
2023-09-24 16:27:26 -0300

    Added get started button to navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -4,12 +4,10 @@ const Navbar = () => {
     <nav className=" h-50 mt-8">
       <div className="w-10/12 flex mx-auto justify-between items-center">
         <div className="flex font-[Narrow] font-bold cursor-pointer">
-          <Link href="/">
-            <b className="text-[#C525FF] mr-2">$</b>Finance Manager Logo
-          </Link>
+          <b className="text-[#C525FF] mr-2">$</b>Finance Manager Logo
         </div>
 
-        <ul className="flex  gap-14 items-center ">
+        <ul className="flex  gap-10 items-center ">
           <li>
             <Link href="/">Home</Link>
           </li>
@@ -25,6 +23,10 @@ const Navbar = () => {
           <li className="px-5 py-2 border rounded-[40px] w-26 border-black">
             {" "}
             <Link href="/login">Sing In</Link>
+          </li>
+          <li className="px-5 py-2 border border-black rounded-[40px] w-26 bg-black text-white -ml-4">
+            {" "}
+            <Link href="/sing-up">Get Started</Link>
           </li>
         </ul>
       </div>
