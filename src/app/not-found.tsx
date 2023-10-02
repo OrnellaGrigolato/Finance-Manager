aec7f8c208d1cde883e2e5832fbafba6ba5ec970
Ornella <nelitagrigolato@gmail.com>
2023-10-02 16:15:10 -0300

    Changing components folder to the rigth position

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Navbar from "../../components/Navbar";
-import Footer from "../../components/Footer";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 import Link from "next/link";
 const ErrorPage = () => {
   return (
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import Navbar from "../../components/Navbar";
-import Header from "../../components/Header";
-import Companies from "../../components/Companies";
-import ProsToUseSection from "../../components/ProsToUseSection";
-import Stadisctics from "../../components/Stadisctics";
-import Testiomonial from "../../components/Testiomonial";
-import CallToAction from "../../components/CallToAction";
-import Footer from "../../components/Footer";
+import Navbar from "../components/Navbar";
+import Header from "../components/Header";
+import Companies from "../components/Companies";
+import ProsToUseSection from "../components/ProsToUseSection";
+import Stadisctics from "../components/Stadisctics";
+import Testiomonial from "../components/Testiomonial";
+import CallToAction from "../components/CallToAction";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
