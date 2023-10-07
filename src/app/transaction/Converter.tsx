7439fae3032a254ab5dac6aaabf83f763ee5931d
Ornella <nelitagrigolato@gmail.com>
2023-10-07 19:09:39 -0300

    Finishing transaction functionallity

--- src/app/transaction/Converter.tsx
+++ src/app/transaction/Converter.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, ChangeEvent } from "react";
 import Country_list from "./data/CountryList";
 import Image from "next/image";
 
@@ -9,7 +9,6 @@ function CurrencyConverter() {
   const [conversionRate, setConversionRate] = useState(0);
   const [conversionResult, setConversionResult] = useState<number>(0);
   const [currencies, setCurrencies] = useState<string[]>([]);
-  const [fetchingData, setFetchingData] = useState(false);
 
   const apiKey = process.env.NEXT_PUBLIC_EXCHANGERATE_APIKEY;
 
@@ -24,22 +23,21 @@ function CurrencyConverter() {
       });
   }, []);
 
-  const handleGetExchange = () => {
-    setFetchingData(true);
+  useEffect(() => {
     fetch(
-      `https://v6.exchangerate-api.com/v6/${apiKey}/pair/${baseCurrency}/${targetCurrency}/${amount}`
+      `https://v6.exchangerate-api.com/v6/${apiKey}/pair/${baseCurrency}/${targetCurrency}`
     )
-      .then((response) =>
-        response.json().then((data) => {
-          setConversionRate(data.conversion_rate);
-          setConversionResult(amount * data.conversion_rate);
-          setFetchingData(false);
-        })
-      )
+      .then((response) => response.json())
+      .then((data) => {
+        setConversionRate(data.conversion_rate);
+      })
       .catch((error) => {
         console.log(error);
-        setFetchingData(false);
       });
+  }, [baseCurrency, targetCurrency]);
+
+  const handleIputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setAmount(Number(e.target.value));
   };
 
   const handleCurrencySwap = () => {
@@ -58,22 +56,24 @@ function CurrencyConverter() {
 
   return (
     <div>
-      <div className="bg-card-bg rounded-3xl p-10 shadow-blackShadow h-[50vh]">
+      <div className="bg-card-bg rounded-3xl p-10 shadow-blackShadow h-[50vh] max-sm:h-fit">
         <div className="flex flex-col gap-2 ">
           <label className="text-sm">You Pay:</label>
           <input
             className="bg-card-bg border-b-2 w-40 border-primary font-bold"
             type="number"
-            value={amount}
-            onChange={(e) => setAmount(Number(e.target.value))}
+            value={amount != 0 ? amount : ""}
+            onChange={(e) => handleIputChange(e)}
+            autoFocus
           />
         </div>
-        <div className="flex justify-between items-center mt-12">
-          <div>
+        <div className="flex justify-between items-center mt-12 max-sm:flex-col max-sm:gap-6">
+          <div className="max-sm:flex max-sm:items-center max-sm:flex-col max-sm:gap-2">
             <label>From:</label>
             <select
               value={baseCurrency}
               onChange={(e) => setBaseCurrency(e.target.value)}
+              className="ml-2 bg-bg rounded-md p-1 mb-1"
             >
               {currencies.map((currency) => (
                 <option key={currency} value={currency}>
@@ -90,19 +90,20 @@ function CurrencyConverter() {
           </div>
           <div>
             <Image
-              src="./exchange-icon.svg"
-              width={30}
-              height={30}
+              src="/swap.png"
+              width={40}
+              height={40}
               alt="Exchange icon"
               onClick={handleCurrencySwap}
-              className="cursor-pointer"
+              className="cursor-pointer rotate-90 max-sm:rotate-0"
             />
           </div>
-          <div>
+          <div className="max-sm:flex max-sm:items-center max-sm:flex-col max-sm:gap-2">
             <label>To:</label>
             <select
               value={targetCurrency}
               onChange={(e) => setTargetCurrency(e.target.value)}
+              className="ml-2 bg-bg rounded-md p-1 mb-1"
             >
               {currencies.map((currency) => (
                 <option key={currency} value={currency}>
@@ -119,27 +120,22 @@ function CurrencyConverter() {
           </div>
         </div>
 
-        {fetchingData ? (
-          <p>Loading...</p>
-        ) : (
-          <div className="w-full text-center mt-12">
-            <p>
-              Conversion Rate: {baseCurrency}/{targetCurrency} ={" "}
-              {conversionRate.toFixed(4)}
-            </p>
-            <p>
-              Result: {amount} {baseCurrency} ={" "}
-              {(amount * conversionResult).toFixed(2)} {targetCurrency}
-            </p>
-          </div>
-        )}
+        <div className="w-full text-center mt-12 max-sm:mt-4">
+          <p className="mb-2">
+            Conversion Rate: {baseCurrency}/{targetCurrency} ={" "}
+            {conversionRate.toFixed(4)}
+          </p>
+          <p>
+            Result: {amount} {baseCurrency} ={" "}
+            {(amount * conversionRate).toFixed(2)} {targetCurrency}
+          </p>
+        </div>
       </div>
       <button
-        onClick={handleGetExchange}
-        disabled={fetchingData}
-        className="bg-gradient-to-b from-primary to-[#391EDC] w-full rounded-[20px] text-white font-bold p-5 mt-10 shadow-blackShadow"
+        onClick={() => alert("Imaginate que tus monedas se cambiaron")}
+        className="bg-gradient-to-b from-primary to-[#391EDC] w-full rounded-[20px] text-white font-bold p-5 mt-10 shadow-blackShadow max-sm:m0b-6"
       >
-        Get Exchange
+        Exchange
       </button>
     </div>
   );
