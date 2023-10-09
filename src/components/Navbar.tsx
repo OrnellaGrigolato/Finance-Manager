39d7398974ed83466492491c7ccafcd13b75354a
TrosseroAgustin01 <trosseroagustin01@gmail.com>
2023-10-09 11:02:02 -0300

    update navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -46,6 +46,7 @@ const Navbar = () => {
 
 
   return (
+    
     <nav className=" h-50 mt-8">
       <div className="w-10/12 flex mx-auto justify-between items-center">
         <div className="flex font-[Narrow] font-bold cursor-pointer">
@@ -53,8 +54,41 @@ const Navbar = () => {
             <b className="text-[#C525FF] mr-2">$</b>Finance Manager Logo
           </Link>
         </div>
+        {session?.user ? (
+          <ul className="flex  gap-10 items-center ">
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/app">App</Link>
+            </li>
+            <li>
+              <Link href="/about">About</Link>
+            </li>
+            <li>
+              <Link href="/contact">Contact</Link>
+            </li>
 
-        <ul className="flex  gap-10 items-center  max-sm:hidden">
+            <img
+              src={
+                session.user.image
+                  ? session.user.image
+                  : "https://img.freepik.com/vector-gratis/ilustracion-icono-avatar-usuario_53876-5907.jpg?w=740&t=st=1696339921~exp=1696340521~hmac=fe51c494eaae6033f6b83f689a78e03af140c873e6b1b914c6575bf7094f1e8f"
+              }
+              alt="user icon"
+              width={30}
+              height={30}
+              className="rounded"
+            />
+            <button
+              className="px-5 py-2 border border-black rounded-[40px] w-26 bg-black text-white -ml-4"
+              onClick={() => signOut()}
+            >
+              Log Out
+            </button>
+          </ul>
+        ) : (
+          <ul className="flex  gap-10 items-center  max-sm:hidden">
           <li>
             <Link href="/">Home</Link>
           </li>
@@ -75,6 +109,7 @@ const Navbar = () => {
             <Link href="/sing-up">Get Started</Link>
           </li>
         </ul>
+        )}
         <div className="hidden max-sm:block">
           {" "}
           <Menu_landing />
