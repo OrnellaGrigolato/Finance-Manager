c79b4738e05327b802fb6b78ea29b0b8d905662c
Ornella <nelitagrigolato@gmail.com>
2023-10-13 16:23:30 -0300

    Showing database data in dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,32 +2,75 @@
 import Navbar from "./Navbar";
 import Button from "./Button";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import WalletCard from "./WalletCard";
 import { useSession } from "next-auth/react";
 import MovementCard from "./MovementCard";
+import getUserDataFromToken from "@/utils/authUtils";
+interface ApiResponse {
+  finder: {
+    id: number;
+    username: string;
+    email: string;
+    password: string;
+    login_date: string;
+    available_money: string;
+    lastmove_amount: string | null;
+    lastmove_date: string | null;
+  };
+  message: string;
+}
 const Dashboard = () => {
-  const { data: session, status } = useSession();
-
-  console.log(session);
   const [isBalanceShowed, setIsBalanceShowed] = useState(true);
-  return (
+  const [userInfo, setUserInfo] = useState<ApiResponse>();
+  const [isFetch, setIsFetch] = useState(false);
+  //Getting token value from cookies
+  const token = document?.cookie
+    ?.split("; ")
+    .find((row) => row.startsWith("token"))
+    ?.split("=")[1];
+  //
+
+  const userData = token ? getUserDataFromToken(token) : null;
+
+  useEffect(() => {
+    userData
+      ? fetch(`/api/users/${userData?.user_id}`)
+          .then((response) => response.json())
+          .then((data) => {
+            setUserInfo(data);
+            setIsFetch(true);
+            console.log(data);
+          })
+          .catch((error) => {
+            console.log(error);
+          })
+      : null;
+  }, []);
+
+  return isFetch ? (
     <div className="bg-bg lg:w-[90vw] lg:ml-[10vw] lg:pt-10">
       <Navbar />
       <p className="mb-5 text-xl font-bold hidden w-11/12 mx-auto lg:block ">
-        Welcome Back, {session?.user?.name?.split(" ")[0]}!
+        Welcome Back, {userInfo?.finder?.username?.split(" ")[0]}!
       </p>
       <div className="bg-gradient-to-b from-primary to-[#391EDC] h-[38vh] w-full rounded-[25px] text-white flex justify-center shadow-blackShadow items-center flex-col lg:w-11/12 lg:mx-auto lg:h-[25vh]">
         <div className="lg:flex lg:justify-between lg:items-center lg:w-4/5">
           <div className="text-center">
             <p className="mb-12 text-xl font-bold lg:hidden">
               {" "}
-              Welcome Back, {session?.user?.name?.split(" ")[0]}!
+              Welcome Back, {userInfo?.finder?.username?.split(" ")[0]}!
             </p>
             <p>Current Wallet Balance</p>
             <div className="flex items-center gap-5 mt-3 lg:mb-5">
               <b className="text-5xl">
-                {isBalanceShowed ? "$ 30,293.2" : "$ ******"}
+                {isBalanceShowed
+                  ? `$ ${userInfo?.finder?.available_money}`
+                  : `$ ${"*".repeat(
+                      userInfo?.finder?.available_money
+                        ? userInfo?.finder?.available_money.length
+                        : 0
+                    )}`}
               </b>
               <button
                 onClick={() => {
@@ -96,7 +139,7 @@ const Dashboard = () => {
         </div>
       </div>
     </div>
-  );
+  ) : null;
 };
 
 export default Dashboard;
---
+++ src/utils/authUtils.ts
@@ -0,0 +1,17 @@
+
+import { decode } from 'jsonwebtoken';
+interface DecodedToken {
+  user_id: number;
+  iat: number;
+  exp: number;
+}
+const getUserDataFromToken = (token:string):DecodedToken |void  => {
+   
+  try {
+    return decode(token) as DecodedToken;   } catch (error) {
+    console.error('Error al decodificar el token:', error);
+
+  }
+}
+
+export default getUserDataFromToken 
