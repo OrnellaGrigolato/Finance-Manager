56b7b402d67be8131b097a691befaaac98af568a
Ornella <nelitagrigolato@gmail.com>
2023-10-16 15:14:55 -0300

    Showing moves from db in dashboard

--- src/app/dashboard/MovementCard.tsx
+++ src/app/dashboard/MovementCard.tsx
@@ -1,7 +1,35 @@
-const MovementCard = () => {
+import { Movement } from "../types/type";
+
+const MovementCard = (props: { props: Movement }) => {
+  const moveInfo = props.props;
   return (
-    <div className="h-10 p-4 border border-black flex items-center shadow-blackShadow">
-      Movement
+    <div className="p-4 border border-black  shadow-blackShadow mb-4">
+      <div className="flex justify-between">
+        <div>
+          <p className="font-bold">{moveInfo.title}</p>
+          <p className="text-sm">{moveInfo.description}</p>
+        </div>
+        <div className="text-right">
+          <p
+            className={
+              moveInfo.discount_amount
+                ? "text-red-500 font-bold"
+                : "text-green-600 font-bold"
+            }
+          >
+            {moveInfo.discount_amount
+              ? "- " + moveInfo.discount_amount
+              : "+ " + moveInfo.income_amount}
+          </p>
+          <p className="text-xs">
+            {moveInfo.movement_date
+              .slice(0, -14)
+              .split("-")
+              .reverse()
+              .join("-")}
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
