1124775d4a49bf48ad643be387aa967ea38c2796
Ornella <nelitagrigolato@gmail.com>
2023-10-21 11:18:59 -0300

    Making log-in responsive and adding some new styles

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,14 +1,15 @@
-"use client"
+"use client";
 
-import Cookies from 'js-cookie'
-import { useRouter } from 'next/navigation';
-import { use, useState } from 'react';
-import Link from 'next/link';
+import Cookies from "js-cookie";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 const Login = () => {
   const [formData, setFormData] = useState({
-    email: '',
-    password: '',
+    email: "",
+    password: "",
   });
 
   const router = useRouter();
@@ -17,27 +18,26 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch('/api/users/login', {
-        method: 'POST',
+      const response = await fetch("/api/users/login", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(formData),
       });
 
       if (response.ok) {
         const data = await response.json();
-        console.log(Cookies.get("token"));
-        
+
         // Redireccionar al usuario a la página deseada después del inicio de sesión.
-         router.push("/dashboard")
+        router.push("/dashboard");
       } else {
         const errorData = await response.json();
         alert(errorData.message);
         console.error(errorData.message);
       }
     } catch (error) {
-      console.error('Error al enviar la solicitud de inicio de sesión', error);
+      console.error("Error al enviar la solicitud de inicio de sesión", error);
     }
   };
 
@@ -51,21 +51,25 @@ const Login = () => {
 
   return (
     <main className="h-[100vh]">
-      <div className="h-full w-full relative flex justify-center items-center">
-        <img
+      <div className="h-full w-full relative flex justify-center items-center ">
+        <Image
+          fill={true}
           src="/login-bg.jpg"
+          priority={true}
           alt=""
-          className="w-full h-full absolute top-0 left-0 -z-10"
+          className="w-full h-full absolute top-0 left-0 -z-10 max-sm:object-cover max-sm:object-left-top"
         />
-        <div className="w-3/12 bg-white px-20 py-14 bg-opacity-70 rounded-[20px]">
-          <h1 className="text-3xl font-extrabold">Welcome Back!</h1>
-          <p className="mt-2 mb-6">Please login to access your account</p>
+        <div className="w-3/12 bg-white px-20 py-14 bg-opacity-70 rounded-[20px] max-sm:w-11/12 max-sm:bg-opacity-80">
+          <h1 className="text-3xl font-bold">Welcome Back!</h1>
+          <p className="mt-2 mb-6 text-sm">
+            Please login to access your account
+          </p>
           <form onSubmit={handleSubmit}>
-            <label className="block" htmlFor="email">
+            <label className="block mb-2" htmlFor="email">
               Email
             </label>
             <input
-              className="block mb-4 p-2 rounded-[30px] bg-[#fafafa]"
+              className="block mb-4 p-2 rounded-[30px] bg-[#f5f5f5] w-full"
               id="email"
               type="email"
               name="email"
@@ -74,11 +78,11 @@ const Login = () => {
               required
               placeholder="example@gmail.com"
             />
-            <label className="block" htmlFor="password">
+            <label className="block mb-2" htmlFor="password">
               Password
             </label>
             <input
-              className="block mb-4 p-2 rounded-[30px] bg-[#f5f5f5]"
+              className="block mb-4 p-2 rounded-[30px] bg-[#f5f5f5] w-full"
               id="password"
               type="password"
               name="password"
@@ -87,16 +91,15 @@ const Login = () => {
               required
               placeholder="Password"
             />
-            <p className="text-primary -mt-3">Forgot Password?</p>
             <button
               type="submit"
-              className="px-7 py-3 text-base mt-8 bg-black text-white rounded-[40px] block"
+              className="px-7 py-3 text-base mt-8 bg-black text-white rounded-[40px] block w-full"
             >
               Log In
             </button>
           </form>
-          <p>
-            Don't have an account?{' '}
+          <p className="mt-3 text-sm">
+            Don't have an account?{" "}
             <Link href="/sign-up" className="text-primary">
               Sign up
             </Link>
