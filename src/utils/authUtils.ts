2756e405d775d5b0f6743a226f7f4ada79c52490
Leandro Viscolungo <leandroviscolungo@gmail.com>
2023-10-31 22:59:40 -0300

    small changes

--- src/utils/authUtils.ts
+++ src/utils/authUtils.ts
@@ -1,20 +1,26 @@
-
 import { decode } from 'jsonwebtoken';
+import Cookies from "js-cookie";
+
 interface DecodedToken {
-  //a veces se devuelve un user_id a veces un id, si re remueve 1 va a haber errores con el token, causa que no se cargue ninguna ruta 
-  user_id: number;
-  id:number; 
-  iat: number;
-  exp: number;
-}
-const getUserDataFromToken = (token:string):DecodedToken |void  => {
-   
-  try {
-    
-    return decode(token) as DecodedToken;   } catch (error) {
-    console.error('Error al decodificar el token:', error);
-
-  }
+ id:number; 
+ iat: number;
+ exp: number;
 }
 
-export default getUserDataFromToken 
+const getUserDataFromToken = (token:string):DecodedToken |void => {
+ try {
+   const decodedToken = decode(token) as DecodedToken;
+   const currentDate = Math.floor(Date.now() / 1000);
+
+   if (decodedToken.exp < currentDate) {
+     Cookies.set("token", '');
+     return;
+   }
+
+   return decodedToken;
+ } catch (error) {
+   console.error('Error al decodificar el token:', error);
+ }
+}
+
+export default getUserDataFromToken;
