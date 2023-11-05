2f6bbfebadd80c049af459a8b609f39f7bb7e2fd
Ornella <nelitagrigolato@gmail.com>
2023-11-05 17:58:21 -0300

    Solving problem: the wallet was not working when the user doesn't have moves

--- src/app/dashboard/Wallet.tsx
+++ src/app/dashboard/Wallet.tsx
@@ -30,7 +30,7 @@ const Wallet = (props: { userId: number }) => {
     fetch(`/api/moves/user/${props.userId}`)
       .then((res) => res.json())
       .then((data) => {
-        setMoves(data.finder);
+        setMoves(data.finder ? data.finder : []);
       })
 
       .catch((error) => {
@@ -41,7 +41,7 @@ const Wallet = (props: { userId: number }) => {
   const getUserCurrencies = useCallback(() => {
     let currenciesIds: number[] = [];
 
-    moves.map((e) => {
+    moves?.map((e) => {
       if (!currenciesIds.includes(e.currency_id)) {
         currenciesIds.push(e.currency_id);
       }
@@ -98,10 +98,11 @@ const Wallet = (props: { userId: number }) => {
   useEffect(() => {
     getMoves();
   }, [getMoves]);
-
   return (
     <div className="flex gap-4 mt-5 flex-wrap">
-      {moves.length != 0 ? (
+      {moves?.length != 0 && userInfo?.available_money === "0" ? (
+        <div>Your wallet is empty.</div>
+      ) : moves?.length != 0 && userInfo?.available_money != "0" ? (
         loading ? (
           <div className="loading my-5 mx-auto"></div>
         ) : (
@@ -118,9 +119,9 @@ const Wallet = (props: { userId: number }) => {
         <div>You have&apos;t deposited money to the platform yet</div>
       )}
 
-      {userInfo?.available_money === "0" && moves.length != 0 ? (
-        <div>Your wallet is empty.</div>
-      ) : null}
+      {/* {userInfo?.available_money === "0" && moves?.length != 0 ? (
+       
+      ) : null} */}
     </div>
   );
 };
