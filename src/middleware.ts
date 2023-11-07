40f89d948b3653f4bf74b34a7b2706f754e04753
Leandro Viscolungo <leandroviscolungo@gmail.com>
2023-11-07 00:09:58 -0300

    translation, middleware and not-found are working

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,43 @@
-//middleware-intl.ts
-import createMiddleware from 'next-intl/middleware';
- 
-export default createMiddleware({
-  // A list of all locales that are supported
-  locales: ['en', 'es'],
- 
-  // If this locale is matched, pathnames work without a prefix (e.g. `/about`)
-  defaultLocale: 'en',
-});
- 
+import createIntlMiddleware from 'next-intl/middleware';
+import { NextRequest, NextResponse } from 'next/server';
+import * as jose from "jose";
+
+export default async function middleware(request: NextRequest) {
+  // Step 1: Use the incoming request
+  const defaultLocale = request.headers.get('x-default-locale') || 'en';
+
+
+  
+  // Step 2: Create and call the next-intl middleware
+  const handleI18nRouting = createIntlMiddleware({
+    locales: ['en', 'es'],
+    defaultLocale
+  });
+  const response = handleI18nRouting(request);
+  // Step 3: Alter the response
+  response.headers.set('x-default-locale', defaultLocale);
+
+  const paths = ["/dashboard", "/es/dashboard", "/wallet", "/es/wallet", "/moves-form", "/es/moves-form", "/transaction", "/es/transaction", "/profile", "/es/profile"];
+  // Check if the request is for /dashboard
+  if (paths.includes(request.nextUrl.pathname)) {
+    try {
+      const userToken = request.cookies.get("token")?.value;
+      if (!userToken) {
+        return NextResponse.redirect(new URL("/login", request.url));
+      }
+
+      const secret = new TextEncoder().encode(process.env.AUTH_SECRET);
+      await jose.jwtVerify(userToken, secret);
+      
+      return NextResponse.next();
+    } catch (error) {
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
+  }
+
+  return response;
+}
+
 export const config = {
   matcher: ['/((?!api|_next|_vercel|.*\\..*).*)', '/reset-password/(.*)']
- };
+};
