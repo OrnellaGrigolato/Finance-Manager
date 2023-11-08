aaa570a55e25f6ccb67a06c5ba656734156c6419
Leandro Viscolungo <leandroviscolungo@gmail.com>
2023-11-08 14:35:25 -0300

    added a response allways

--- src/app/api/users/verify/route.ts
+++ src/app/api/users/verify/route.ts
@@ -9,46 +9,51 @@ export async function GET(req: Request) {
   const url = req.url || "";
   const token = url.split("=")[1];
   const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL || "http://localhost:3000";
-
+ 
   if (!token) {
     return NextResponse.json({ message: "missing fields" }, { status: 400 });
   }
-    let userId;
-
+ 
+  let userId;
+ 
+  try {
+    const decoded = verify(
+      token as string,
+      process.env.AUTH_SECRET as string
+    ) as { id: string };
+    userId = decoded.id;
+  } catch (error) {
+    return NextResponse.json({ error: "Invalid token" }, { status: 400 });
+  }
+ 
+  let retries = 0;
+  const maxRetries = 5;
+ 
+  for (let i = 0; i < maxRetries; i++) {
     try {
-      const decoded = verify(
-        token as string,
-        process.env.AUTH_SECRET as string
-      ) as { id: string };
-      userId = decoded.id;
+      await prisma.users.update({
+        where: { id: Number(userId) },
+        data: { emailVerified: true },
+      });
+ 
+      return NextResponse.redirect(`${baseUrl}/validate`);
     } catch (error) {
-      return NextResponse.json({ error: "Invalid token" }, { status: 400 });
-    }
-
-    let retries = 0;
-    const maxRetries = 5;
-
-    for (let i = 0; i < maxRetries; i++) {
-      try {
-        await prisma.users.update({
-          where: { id: Number(userId) },
-          data: { emailVerified: true },
-        });
-
-        return NextResponse.redirect(`${baseUrl}/validate`);
-      } catch (error) {
-        console.log(error);
-        retries++;
-        if (retries === maxRetries) {
-          return NextResponse.json(
-            { error: "Error verifying email" },
-            { status: 500 }
-          );
-        }
+      console.log(error);
+      retries++;
+      if (retries === maxRetries) {
+        return NextResponse.json(
+          { error: "Error verifying email" },
+          { status: 500 }
+        );
       }
     }
+  }
  
-}
+  // Si llegamos hasta aquí, significa que no se encontró un token válido
+  // y no se pudo verificar el correo electrónico. Por lo tanto, retornamos un error.
+  return NextResponse.json({ error: "No valid token found" }, { status: 400 });
+ }
+ 
 
 export async function POST(req: Request) {
   
